d63b0b59d98b85e3c17d528aa3826377e605d7d2
mariazhingri <mariazhingripaz@outlook.com>
2023-11-15 23:00:44 -0500

    Actividad_Perfil_Usuario_By_Maria

---
+++ app/src/main/java/com/example/comentarios_valoracion_scarletgg/Perfil_UsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.comentarios_valoracion_scarletgg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_UsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
